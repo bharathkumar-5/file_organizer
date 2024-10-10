c80e6cc2b409427c994b5d267512a777f8c2bef7
Bharath Marley <bharathmarley103@gmail.com>
2024-10-11 01:18:31 +0530

    style added

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// const files = [
-//     'document1.txt', 'presentation1.pdf', 'song1.mp3', 'installer1.exe', 'archive1.rar',
-//     'report1.docx', 'image1.jpg', 'graphic1.png', 'animation1.gif', 'compressed1.zip',
-//     'document2.txt', 'presentation2.pdf', 'song2.mp3', 'installer2.exe', 'archive2.rar',
-//     'report2.docx', 'image2.jpg', 'graphic2.png', 'animation2.gif', 'compressed2.zip',
-//     null, 'presentation3.pdf', '', 'installer3.exe', 'archive3.rar',
-//     'report3.docx', 'image3.jpg', 'graphic3.png', 'animation3.gif', 'compressed3.zip',
-//     'document4.txt', 'presentation4.pdf', 'song4.mp3', 'installer4.exe', 'archive4.rar',
-//     'report4.docx', 'image4.jpg', 'graphic4.png', 'animation4.gif', 'compressed4.zip',
-//     'document5.txt', 'presentation5.pdf', 'song5.mp3', 'installer5.exe', 'archive5.rar',
-//     'report5.docx', 'image5.jpg', 'graphic5.png', 'animation5.gif', 'compressed5.zip',
-//     'document6.txt', 'presentation6.pdf', 'song6.mp3', 'installer6.exe', 'archive6.rar',
-//     'report6.docx', 'image6.jpg', null, 'animation6.gif', 'compressed6.zip',
-//     'document7.txt', 'presentation7.pdf', 'song7.mp3', 'installer7.exe', 'archive7.rar',
-//     'report7.docx', 'image7.jpg', 'graphic7.png', 'animation7.gif', 'compressed7.zip',
-//     'document8.txt', 'presentation8.pdf', 'song8.mp3', 'installer8.exe', 'archive8.rar',
-//     'report8.docx', 'image8.jpg', '', 'animation8.gif', 'compressed8.zip',
-//     'document9.txt', 'presentation9.pdf', 'song9.mp3', 'installer9.exe', 'archive9.rar',
-//     'report9.docx', 'image9.jpg', '', 'animation9.gif', 'compressed9.zip',
-//     'document10.txt', 'presentation10.pdf', 'song10.mp3', 'installer10.exe', 'archive10.rar',
-//     'report10.docx', 'image10.jpg', 'graphic10.png', 'animation10.gif', 'compressed10.zip',
-//   ];
-
 const files = [
     { id: 1 ,name: 'document1.txt',type: 'txt'},
     { id: 2 ,name: 'presentation1.pdf',type: 'pdf'},
@@ -33,7 +10,7 @@ const files = [
     { id: 9 ,name: 'animation1.gif',type: 'gif'},
     { id: 10 ,name: 'compressed1.zip',type: 'zip'},
 
-    { id: 11 ,name: 'null',type: 'txt'},
+    { id: 11 ,name: 'document2.txt',type: 'txt'},
     { id: 12 ,name: 'presentation2.pdf',type: 'pdf'},
     { id: 13 ,name: 'song2.mp3',type: 'mp3'},
     { id: 14 ,name: 'installer2.exe',type: 'exe'},
@@ -84,7 +61,7 @@ const files = [
     { id: 55 ,name: 'archive6.rar',type: 'rar'},
     { id: 56 ,name: 'report6.docx',type: 'docx'},
     { id: 57 ,name: 'image6.jpg',type: 'jpg'},
-    { id: 58 ,name: 'null',type: 'png'},
+    { id: 58 ,name: 'graphic6.png',type: 'png'},
     { id: 59 ,name: 'animation6.gif',type: 'gif'},
     { id: 60 ,name: 'compressed6.zip',type: 'zip'},
 
@@ -146,7 +123,7 @@ function displayFolders(){
     folders.forEach(folder => {
         const div = document.createElement('div');
         div.className = 'folder';
-        div.textContent = `.${folder}`;
+        div.textContent = `${folder}`;
         div.onclick = () => displayFiles(folder);
         folderContainer.appendChild(div);
     });
@@ -167,7 +144,7 @@ function updateFileList() {
     fileListContainer.innerHTML = '';
     filteredFiles.forEach(file => {
         const li = document.createElement('li');
-        li.textContent = `${file.name} (${file.type})`;
+        li.textContent = `${file.name} `;
         fileListContainer.appendChild(li);
     })
 }
